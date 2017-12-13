3135217f7c8e51e7278c7e1f40f4a371ef220322
Alexander Hartson <alexander-hartson@live.com>
2017-12-12 18:16:21 -0600

    adding code

--- date.cpp
+++ date.cpp
@@ -1,1 +1,11 @@
 #include "std_lib_facilities.h"
+
+//simple Date
+struct Date
+{
+    int y; //year
+    int m; //month in a year
+    int d; //day of month
+};
+
+Date today; //a Date variable (a named object)
