43cbf16e917a52df407e425fed26cfe707fa84c8
Alexander Hartson <alexander-hartson@live.com>
2017-12-13 16:40:48 -0600

    code fix

--- date.cpp
+++ date.cpp
@@ -12,28 +12,3 @@ struct Date
 
 Date today; //a Date variable (a named object)
 
-//helper functions:
-
-void int_day(Date& dd, int y, int m, int d)
-{
-    //check that (y,m,d) is a valid date
-    //if it is, use it to initialize dd
-}
-
-void add_day(Date& dd, int n)
-{
-    //increse dd by n days
-}
-
-void f()
-{
-    Date today;
-    cout << today << '\n'; //use today
-    int_day(today, 2008, 3, 30);
-    Date tomorrow;
-    tomorrow.y = today.y;
-    tomorrow.m = today.m;
-    tomorrow.d = today.d+1; //add 1 to today
-    cout << tomorrow << '\n';
-    add_day(today, 1);
-}
