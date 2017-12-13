e36f91d4e0e55eff95e71b12d06af09680776897
Alexander Hartson <alexander-hartson@live.com>
2017-12-13 16:37:54 -0600

    editing struct

--- date.cpp
+++ date.cpp
@@ -1,11 +1,13 @@
 #include "std_lib_facilities.h"
 
 //simple Date
+//guarantee initialization with constuctor
+//provide some notational convenience 
 struct Date
 {
-    int y; //year
-    int m; //month in a year
-    int d; //day of month
+    int y, m, d; //year, month, day
+    Date(int y, int m, int d); //check the valid date and initialize
+    void add_day(int n); //increase the Date by n days
 };
 
 Date today; //a Date variable (a named object)
