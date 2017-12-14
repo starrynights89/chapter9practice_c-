811dc455700b727692367459c39692e016002353
Alexander Hartson <alexander-hartson@live.com>
2017-12-13 21:13:06 -0600

    adding code

--- date.cpp
+++ date.cpp
@@ -5,13 +5,26 @@
 //provide some notational convenience 
 class Date
 {
-private:
-    int y, m, d; //year, month, day
 public:
     Date(int y, int m, int d); //check the valid date and initialize
     void add_day(int n); //increase the Date by n days
     int month() { return m; } 
     int day() { return d; }
     int year() { return y; }
+private:
+    int y, m, d; //year, month, day
 };
 
+Date::Date(int yy, int mm, int dd) //constructor
+:y{yy}, m{mm}, d{dd} //note: member initializers
+{
+}
+
+void Date::add_day(int n)
+{
+}
+
+int month() //oops: we forgot Date::
+{
+    return m; //not the member function, can't access m 
+}
