4714e079d296b9f61a5c0855f6117ab3bcf602d4
Alexander Hartson <alexander-hartson@live.com>
2017-12-13 18:12:43 -0600

    change to class

--- date.cpp
+++ date.cpp
@@ -3,12 +3,20 @@
 //simple Date
 //guarantee initialization with constuctor
 //provide some notational convenience 
-struct Date
+class Date
 {
     int y, m, d; //year, month, day
+public:
     Date(int y, int m, int d); //check the valid date and initialize
     void add_day(int n); //increase the Date by n days
+    int month() { return m; } 
+    int day() { return d; }
+    int year() { return y; }
 };
 
-Date today; //a Date variable (a named object)
-
+void f()
+{
+    Date last(2000, 12, 31);
+    last.add_day(1);
+    add_day(2);
+}
