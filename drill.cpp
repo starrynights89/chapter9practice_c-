0dc8f2f3921d91b2b57913bfeed9b273de521ffc
Alexander Hartson <alexander-hartson@live.com>
2017-12-18 18:00:05 -0600

    code fix

--- drill.cpp
+++ drill.cpp
@@ -1,18 +1,18 @@
 //Chapter 09 drill
 
-#include "std_lib_facilities.h"
 #include "Chrono.h"
+#include <iostream>
 
 void chapter941()
 {
     using namespace Chrono941;
-    cout << "Chapter 9.4.1:\n";
+    std::cout << "Chapter 9.4.1:\n";
     Date today;
     init_day(today,1978,6,25);
     Date tomorrow = today;
     add_day(tomorrow,1);
-    cout << "Today: " << today << endl;
-    cout << "Tomorrow: " << tomorrow << endl;
+    std::cout << "Today: " << today << std::endl;
+    std::cout << "Tomorrow: " << tomorrow << std::endl;
     Date invalid_date;
     init_day(invalid_date,2004,13,-5);
 }
@@ -23,12 +23,12 @@ try
     chapter941();
 }
 
-catch (exception& e)
+catch (std::exception& e)
 {
-    cerr << "exception: " << e.what() << endl;
+    std::cerr << "exception: " << e.what() << std::endl;
 }
 
 catch(...)
 {
-    cerr << "exception\n";
+    std::cerr << "exception\n";
 }
